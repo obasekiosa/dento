f843ce58a54cda69a34fa9aacf85f3d3f53c0ef0
Osakpolor Obaseki <obasekiosa@gmail.com>
2023-11-08 17:55:50 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -3,17 +3,31 @@
 set -o errexit
 
 export PHX_SERVER=true  
+# # Initial setup
+# mix deps.get #--only prod
+# MIX_ENV=prod mix compile
+
+# mix ecto.migrate
+
+# # Compile assets
+# mix phx.digest
+
+# # Build the release and overwrite the existing release directory
+# MIX_ENV=prod mix release --overwrite
+
+
+#  ./_build/prod/rel/dento/bin/dento start 
+
+
 # Initial setup
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 
-mix ecto.migrate
-
 # Compile assets
-mix phx.digest
+MIX_ENV=prod mix assets.deploy
 
-# Build the release and overwrite the existing release directory
-MIX_ENV=prod mix release --overwrite
+# Custom tasks (like DB migrations)
+MIX_ENV=prod mix ecto.migrate
 
-
- ./_build/prod/rel/dento/bin/dento start 
+# Finally run the server
+MIX_ENV=prod mix phx.server
